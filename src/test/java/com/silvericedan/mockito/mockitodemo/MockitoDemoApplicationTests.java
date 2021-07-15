aa3d83b2909e38e65fe74b172599fb7214ba3ef0
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-15 15:48:46 -0300

    initial commit

---
+++ src/test/java/com/silvericedan/mockito/mockitodemo/MockitoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvericedan.mockito.mockitodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

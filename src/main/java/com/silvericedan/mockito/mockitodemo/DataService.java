87497b34382794257b15e083dedab536e7152339
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-15 15:56:56 -0300

    added DataService and SomeBusinessImpl

---
+++ src/main/java/com/silvericedan/mockito/mockitodemo/DataService.java
@@ -0,0 +1,5 @@
+package com.silvericedan.mockito.mockitodemo;
+
+public interface DataService {
+    int[] retrieveAllData();
+}
---
+++ src/main/java/com/silvericedan/mockito/mockitodemo/SomeBusinessImpl.java
@@ -0,0 +1,18 @@
+package com.silvericedan.mockito.mockitodemo;
+
+public class SomeBusinessImpl {
+
+    private DataService dataService;
+
+    int findTheGreatestFromAllData(){
+        int[] data = dataService.retrieveAllData();
+        int greatest = Integer.MIN_VALUE;
+
+        for( int value : data){
+            if (value > greatest){
+                greatest = value;
+            }
+        }
+        return greatest;
+    }
+}
